9e23e33fad54d9bde1ef7dbcc363f9d56de8175a
sherzoDev <mustafoqulovsherzodbek@gmail.com>
2022-09-07 14:26:02 +0500

    Intial commit

--- js/app.js
+++ js/app.js
@@ -2,5 +2,5 @@ const header = document.querySelector(".siteheader");
 const header__btn = document.querySelector(".siteheader__btn");
 
 header__btn.addEventListener("click", function () {
-    header.classList.toggle("site-nav-on");
+    document.body.classList.toggle("site-nav-on");
 })
